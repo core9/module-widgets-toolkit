be694a95304656b5014b6e5dbc255a9faf535e5a
jessec <robertjessecooke@gmail.com>
2014-04-06 21:50:03 +0200

    remove login widget

--- src/impl/java/io/core9/plugin/session/login/LoginWidget.java
+++
@@ -1,9 +0,0 @@
-package io.core9.plugin.session.login;
-
-import io.core9.core.plugin.Core9Plugin;
-import io.core9.plugin.widgets.pagemodel.PageModelProvider;
-import io.core9.plugin.widgets.widget.WidgetProvider;
-
-public interface LoginWidget extends WidgetProvider, Core9Plugin, PageModelProvider {
-
-}
